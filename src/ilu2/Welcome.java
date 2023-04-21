d94f59773d1deb128b644e67a6abe41ae7099c66
GabGRZT <gabriel.grezit@gmail.com>
2023-04-21 15:07:45 +0200

    EX_3

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -3,10 +3,13 @@ package ilu2;
 public class Welcome {
 	
 	public static String welcome(String input) {
-		StringBuilder chaine = new StringBuilder("Hello, ");
-		if (input == null||input.trim().equals("")) return chaine.append("my friend").toString();
+		StringBuilder chaine = new StringBuilder("");
+		if (input == null||input.trim().equals("")) return chaine.append("Hello, my friend").toString();
+		if(input == input.toUpperCase()) {
+			return chaine.append("HELLO, "+input).toString();
+		}
 		input = input.substring(0,1).toUpperCase() + input.substring(1);
-		return chaine.append(input).toString();
+		return chaine.append("Hello "+input).toString();
 		
 	}
 }
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -8,13 +8,18 @@ class WelcomeTest {
 
 	@Test
 	void test_ex_1() {
-		assertEquals("Hello, Bob",Welcome.welcome("bob"));
+		assertEquals("Hello Bob",Welcome.welcome("bob"));
 	}
 	
 	@Test
 	void test_ex_2() {
 		assertEquals("Hello, my friend",Welcome.welcome(null));
 		assertEquals("Hello, my friend",Welcome.welcome(" "));
+	}
+	
+	@Test
+	void test_ex_3() {
+		assertEquals("HELLO, JERRY", Welcome.welcome("JERRY"));
 	}
 	
 
