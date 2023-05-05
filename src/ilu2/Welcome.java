71336b2da6d096c8f92ccd9180128018a0cf569d
GabGRZT <114427044+GabGRZT@users.noreply.github.com>
2023-05-05 23:19:31 +0200

    EX_7 ok: ajout de 5 nouvelles fonctions

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -2,60 +2,114 @@ package ilu2;
 
 
 public class Welcome {
-	
+
 	//Fonction qui renvoie le mot avec la premiere lettre en majuscule
 	private static String giveUpperCase(String input) {
 		return input.substring(0,1).toUpperCase() + input.substring(1);
 	}
 	
-	private static StringBuilder setChaineMin (String input) {
-		String [] noms = input.split(",");
-		StringBuilder chaineMin = new StringBuilder("Hello");
-		for(int i = 0; i<noms.length; i++) {
-			noms[i] = noms[i].trim();
-			if (!(noms[i].equals(noms[i].toUpperCase()))) {
-				noms[i] = giveUpperCase(noms[i]);
-				chaineMin.append(", "+noms[i]);
+	//Fonction qui renvoie true si le mot entre en parametre est en majuscule sinon false
+	private static boolean IsStringMaj(String nom) {
+		return nom.equals(nom.toUpperCase());
+	}
+	
+	private static String TwoNamesInput(String input) {
+		String[] noms = input.split(",");
+		StringBuilder chaine = new StringBuilder ("");
+		return chaine.append("Hello, "+giveUpperCase(noms[0].trim())+", "+giveUpperCase(noms[1].trim())).toString();
+	}
+	
+	//Fonction qui renvoie un tableau de String contenant les noms en minuscules.
+	private static String[] tabLowerString(String input) {
+		String[] noms = input.split(",");
+		int nbNomsMin = 0;
+		for (int i = 0; i< noms.length;i++) {
+			if(!IsStringMaj(noms[i]))nbNomsMin++;
+		}
+		String[] lowercaseString = new String[nbNomsMin];
+		int indicelowercaseString = 0;
+		for (int i = 0; i < noms.length; i++) {
+			if(!IsStringMaj(noms[i])) {
+				lowercaseString[indicelowercaseString]=giveUpperCase(noms[i]).trim(); 
+				indicelowercaseString++;
 			}
+		}
+		return lowercaseString;
+	}
+	
+	//Fonction qui renvoie un tableau de String contenant les noms en minuscules.
+	private static String[] tabUpperString(String input) {
+		String[] noms = input.split(",");
+		int nbNomsMaj = 0;
+		for (int i = 0; i< noms.length;i++) {
+			if(IsStringMaj(noms[i])) nbNomsMaj++;
+		}
+		String[] uppercaseString = new String[nbNomsMaj];
+		int indiceUppercaseString = 0;
+		for (int i = 0; i < noms.length; i++) {
+			if(IsStringMaj(noms[i])) {
+				uppercaseString[indiceUppercaseString]=noms[i];
+				indiceUppercaseString ++;
+			}
+		}
+		return uppercaseString;
+	}
+
+	//Fonction qui renvoie la chaine avec tous les noms minuscules avec and.
+	private static StringBuilder appendChaineLowercase(String input) {
+		String[] nomsMin = tabLowerString(input);
+		StringBuilder chaineMin = new StringBuilder("");
+		if ( nomsMin.length == 1) {
+			chaineMin.append(", "+giveUpperCase(nomsMin[0].trim()));
+		}else if (nomsMin.length >= 2) {
+			for (int i = 0; i < nomsMin.length-1; i++) {
+				chaineMin.append(", "+giveUpperCase(nomsMin[i]).trim());
+			}
+			chaineMin.append(" and "+giveUpperCase(nomsMin[nomsMin.length-1]).trim());
 		}
 		return chaineMin;
 	}
-	
-	private static StringBuilder setChaineMaj (String input) {
-		String [] noms = input.split(",");
-		StringBuilder chaineMaj = new StringBuilder(". AND HELLO");
-		for(int i = 0; i<noms.length; i++) {
-			noms[i] = noms[i].trim();
-			if (noms[i].equals(noms[i].toUpperCase())) {
-				noms[i] = giveUpperCase(noms[i]);
-				chaineMaj.append(", "+noms[i]);
+		
+	//Fonction qui renvoie la chaine avec tous les noms majuscules avec and.
+	private static StringBuilder appendChaineUppercase(String input) {
+		String[] nomsMaj = tabUpperString(input);
+		StringBuilder chaineMaj = new StringBuilder("");
+		if ( nomsMaj.length == 1) {
+			chaineMaj.append(", "+nomsMaj[0].trim()+" !");
+		}else if (nomsMaj.length > 1) {
+			for (int i = 0; i < nomsMaj.length-1; i++) {
+				chaineMaj.append(", "+nomsMaj[i].trim());
 			}
-		}
-		if (!(chaineMaj.toString().equals(". AND HELLO"))) {
-			chaineMaj.append(" !");
-		}else {
-			chaineMaj.setLength(0);
+			chaineMaj.append(" AND "+nomsMaj[nomsMaj.length-1].trim()+" !");
 		}
 		return chaineMaj;
+
 	}
 	
 	public static String welcome(String input) {
 		StringBuilder chaine = new StringBuilder("");
-		
+
 		if (input == null||input.trim().equals("")) return chaine.append("Hello, my friend").toString();
-		
-		if(input.equals(input.toUpperCase())) return chaine.append("HELLO, "+input).toString();
-		
+
 		String [] noms = input.split(",");
-		if(noms.length > 1) {
-			StringBuilder chaineMin = setChaineMin(input);
-			StringBuilder chaineMaj = setChaineMaj(input);
-			chaineMin.append(chaineMaj).toString();
-			return chaineMin.toString();
+		
+		if(noms.length == 1 && !IsStringMaj(noms[0])) return chaine.append("Hello, "+giveUpperCase(noms[0])).toString();
+		
+		if(noms.length == 1 && IsStringMaj(noms[0])) return chaine.append("HELLO, "+input).toString();
+		
+		if (noms.length == 2 && !IsStringMaj(noms[0]) && !IsStringMaj(noms[1])) return TwoNamesInput(input) ;
+				
+		StringBuilder chaineMin = new StringBuilder("Hello");
+		chaineMin.append(appendChaineLowercase(input));
+		
+		StringBuilder chaineMaj = new StringBuilder(". AND HELLO");
+		chaineMaj.append(appendChaineUppercase(input));
+		
+		String[] tabMaj =tabUpperString(input);
+		
+		if (noms.length >= 3) {
+			if(tabMaj.length > 0) return chaineMin.append(chaineMaj).toString();
 		}
-		
-		input = giveUpperCase(input);
-		return chaine.append("Hello "+input).toString();
-		
+		return chaineMin.toString();
 	}
 }
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -5,15 +5,15 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class WelcomeTest {
-	
+
 	@Test
 	void test_ex_1() {
-		assertEquals("Hello Bob",Welcome.welcome("bob"));
-		assertEquals("Hello Gab",Welcome.welcome("gab"));
-		assertNotEquals("Hello Gab",Welcome.welcome("bob"));
-		assertNotEquals("Hello BOB",Welcome.welcome("bob"));
+		assertEquals("Hello, Bob",Welcome.welcome("bob"));
+		assertEquals("Hello, Gab",Welcome.welcome("gab"));
+		assertNotEquals("Hello, Gab",Welcome.welcome("bob"));
+		assertNotEquals("Hello, BOB",Welcome.welcome("bob"));
 	}
-	
+
 	@Test
 	void test_ex_2() {
 		assertEquals("Hello, my friend",Welcome.welcome(null));
@@ -22,7 +22,7 @@ class WelcomeTest {
 		assertNotEquals("Hello, bob",Welcome.welcome("  "));
 
 	}
-	
+
 	@Test
 	void test_ex_3() {
 		assertEquals("HELLO, JERRY", Welcome.welcome("JERRY"));
@@ -31,7 +31,7 @@ class WelcomeTest {
 		assertNotEquals("GOODBYE, BOB",Welcome.welcome("BOB"));
 
 	}
-	
+
 	@Test
 	void test_ex_4() {
 		assertEquals("Hello, Amy, Bob", Welcome.welcome("amy,bob"));
@@ -39,27 +39,32 @@ class WelcomeTest {
 		assertNotEquals("Hello, Gab, bob",Welcome.welcome("Gab,bob"));
 		assertNotEquals("Hello, Gab",Welcome.welcome("gab,bob"));
 	}
-	
+
 	@Test
 	void test_ex_5() {
-		assertEquals("Hello, Amy, Bob, Jerry", Welcome.welcome("Amy, bob,jerry"));
-		assertEquals("Hello, Gab, Bob, Luca",Welcome.welcome("Gab, bob,luca"));
+		assertEquals("Hello, Amy, Bob and Jerry", Welcome.welcome("Amy, bob,jerry"));
+		assertEquals("Hello, Gab, Bob and Luca",Welcome.welcome("Gab, bob,luca"));
 		assertNotEquals("Hello, amy, Bob, JERRY",Welcome.welcome("amy,bob,jerry"));
 		assertNotEquals("Hello, amy, gab, jERRY",Welcome.welcome("amy,bob,jerry"));
 
 	}
-	
+
 	@Test 
 	void test_ex_6() {
-		assertEquals("Hello, Amy, Jerry. AND HELLO, BOB !", Welcome.welcome("Amy, BOB, Jerry"));
-		assertEquals("Hello, Jerry. AND HELLO, AMY, BOB !", Welcome.welcome("AMY, BOB, Jerry"));
-		assertEquals("Hello, Jerry, Amy, Bob. AND HELLO, BOB !", Welcome.welcome("jerry,amy,BOB,bob"));
-
+		assertEquals("Hello, Amy and Jerry. AND HELLO, BOB !", Welcome.welcome("Amy, BOB, Jerry"));
+		assertEquals("Hello, Jerry. AND HELLO, AMY AND BOB !", Welcome.welcome("Jerry, AMY, BOB"));
+		assertEquals("Hello, Jerry, Amy and Bob. AND HELLO, BOB !", Welcome.welcome("jerry,amy,BOB,bob"));
 	}
 	
-//	@Test
-//	void test_ex_7() {
-//		assertEquals("Hello, bob and jerry. AND HELLO AMY AND JACK !", Welcome.welcome("bob, AMY, jerry, JACK"));
-//	}
-
+	@Test
+	void test_ex_7() {
+		assertEquals("Hello, Bob, Amy and Jerry", Welcome.welcome("bob, amy, jerry"));
+		assertEquals("Hello, Bob and Jerry. AND HELLO, AMY AND JACK !", Welcome.welcome("bob, AMY, jerry, JACK"));
+	}
+	
+	@Test
+	void test_ex_8() {
+		assertEquals("Hello, Amy, Bob", Welcome.welcome("     amy,    bob  "));
+		assertEquals("Hello, Bob, Jerry and Amy. AND HELLO, JACK !", Welcome.welcome("bob, jerry  ,      amy     , JACK   "));
+	}
 }
